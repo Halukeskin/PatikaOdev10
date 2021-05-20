9f9952a325704652c84debf2c652833bed18092d
Keskin6161 <76918333+Keskin6161@users.noreply.github.com>
2021-05-20 13:15:08 +0300

    Add files via upload

---
+++ donguler2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace donguler2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Bir Sayı Giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac ++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            Console.WriteLine("Bir Harf Giriniz");
+            char karakter = 'a';
+            while (karakter <= 'z')
+            {
+                Console.WriteLine(karakter);
+                karakter ++;
+            }
+
+            string[] arabalar = {"bmw", "ford", "ferrari"};
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
